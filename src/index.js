c5157b1c4470928921416108a2d419b9ce083e79
German Castro <germancastromontes@gmail.com>
2020-02-27 16:06:07 +0100

    bodyparser and helmet

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
